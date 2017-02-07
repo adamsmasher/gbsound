e8340281f2d63ffd9aca0beb47027350e3679a87
Adam Smith <unfactorable.polynomial@gmail.com>
2017-02-07 16:27:02 -0500

    Update Importer.cpp

--- famitracker/Importer.cpp
+++ famitracker/Importer.cpp
@@ -122,7 +122,7 @@ static const char* CT[CT_COUNT] = {
   "ROW",
 };
 
-Command Importer::getCommandEnum(std::string command) {
+Command Importer::getCommandEnum(std::string command) const {
   for (int c = 0; c < CT_COUNT; ++c) {
     if (0 == strcasecmp(command.c_str(), CT[c])) {
         return (Command)c;
@@ -130,7 +130,7 @@ Command Importer::getCommandEnum(std::string command) {
   }
   
   errMsg << "Unrecognized command at line " << t.getLine() << ": '" << command << "'.";
-	throw errMsg.str();
+  throw errMsg.str();
 }
 
 // =============================================================================
